2adcabcc0db1e6774b70991e0d10f3fa1064e44e
Bryson Bloom <brysonbloom@icloud.com>
2023-01-23 16:08:12 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+clicks = 0
+clicks2 = 0
+function button1(){
+    clicks++
+    document.getElementById('clicks').innerHTML = clicks;
+}
+function button2(){
+    clicks2++
+    document.getElementById('clicks2').innerHTML = clicks2;
+}
